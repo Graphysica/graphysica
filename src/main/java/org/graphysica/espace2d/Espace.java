397ceec1930e36ba1e99744c4e53254c04e2473b
Marc-Antoine Ouimet <1622587@li-h015-0t09>
2018-02-12 11:57:01 -0500

    Modélisation de l'échelle et de l'origine virtuelles de l'espace

--- src/main/java/org/graphysica/espace2d/Espace.java
+++ src/main/java/org/graphysica/espace2d/Espace.java
@@ -16,7 +16,11 @@
  */
 package org.graphysica.espace2d;
 
+import com.sun.istack.internal.NotNull;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.layout.AnchorPane;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 /**
  * Un espace est un panneau duquel l'utilisateur peut modifier les composantes
@@ -30,6 +34,20 @@ public class Espace extends AnchorPane {
      * La toile des formes de l'espace.
      */
     private final ToileFormes toile;
+
+    /**
+     * L'échelle de l'espace exprimée en pixels par mètre.
+     */
+    private final ObjectProperty<Vector2D> echelle
+            = new SimpleObjectProperty<>(new Vector2D(100, 100));
+
+    /**
+     * L'origine virtuelle de l'espace exprimée en pixels selon l'origine de
+     * l'écran. Par défaut, l'origine de l'espace est au centre du panneau.
+     */
+    private final ObjectProperty<Vector2D> origine
+            = new SimpleObjectProperty<>(
+                    new Vector2D(getWidth() / 2, getHeight() / 2));
 
     /**
      * Construit un espace initialisé dont les dimensions sont liées à celles de
@@ -54,6 +72,30 @@ public class Espace extends AnchorPane {
     private void lierDimensions() {
         toile.widthProperty().bind(widthProperty());
         toile.heightProperty().bind(heightProperty());
+    }
+
+    public ObjectProperty<Vector2D> echelleProperty() {
+        return echelle;
+    }
+
+    public Vector2D getEchelle() {
+        return echelle.getValue();
+    }
+
+    public void setEchelle(@NotNull final Vector2D echelle) {
+        this.echelle.setValue(echelle);
+    }
+
+    public ObjectProperty<Vector2D> origineProperty() {
+        return origine;
+    }
+
+    public Vector2D getOrigine() {
+        return origine.getValue();
+    }
+
+    public void setOrigine(@NotNull final Vector2D origine) {
+        this.origine.setValue(origine);
     }
 
 }
