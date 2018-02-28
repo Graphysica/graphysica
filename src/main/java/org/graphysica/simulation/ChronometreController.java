63b91aa4725ab38aceec893c440f60f34e00b11c
Victor Babin <victor@vicbab.me>
2018-02-28 15:50:55 -0500

    Ajout de la vue (#5)
    * Création d'une barre de menu
    
    * Organisation des packages de la vue
    
    * Modélisation de l'édition d'un paramètre
    
    * Début de la modélisation de la simulation
    
    * Affichage de l'espace et d'une partie de l'environnement.

---
+++ src/main/java/org/graphysica/simulation/ChronometreController.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Graphysica
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.graphysica.simulation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Victor Babin <vicbab@Graphysica>
+ */
+public class ChronometreController implements Initializable {
+
+    @FXML
+    private Label etiquetteChrono;
+
+    @FXML
+    private Button btnRembobiner;
+
+    @FXML
+    private ImageView imgRembobiner;
+
+    @FXML
+    private Button btnPlayPause;
+
+    @FXML
+    private ImageView imgPlayPause;
+
+    @FXML
+    private Button btnAvancer;
+
+    @FXML
+    private ImageView imgAvancer;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
