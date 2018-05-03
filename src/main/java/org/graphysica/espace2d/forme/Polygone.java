3ca9532ffda43958f66827c1161fd47ec8b3f79c
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2018-05-03 08:41:25 -0400

    Redéfinition de l'ensemble des points d'un polygone

--- src/main/java/org/graphysica/espace2d/forme/Polygone.java
+++ src/main/java/org/graphysica/espace2d/forme/Polygone.java
@@ -19,7 +19,6 @@ package org.graphysica.espace2d.forme;
 import com.sun.istack.internal.NotNull;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.Set;
 import javafx.beans.property.ObjectProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -43,7 +42,8 @@ public class Polygone extends Aire {
      * L'ensemble ordonné des points de coordonnées réelles délimitant ce
      * polygone.
      */
-    private final Set<ObjectProperty<Position>> points = new LinkedHashSet<>();
+    private final LinkedHashSet<ObjectProperty<Position>> points 
+            = new LinkedHashSet<>();
 
     /**
      * Construit un polygone aux points non-définis.
