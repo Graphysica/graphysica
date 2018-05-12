93dbaaeef9b56c099abafdfc341e900d9946c151
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2018-05-12 12:14:22 -0400

    Ajustement du contrôle des données des espaces

--- src/main/java/org/graphysica/Application.java
+++ src/main/java/org/graphysica/Application.java
@@ -3,23 +3,33 @@ package org.graphysica;
 import com.sun.istack.internal.NotNull;
 import static javafx.application.Application.launch;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.graphysica.construction.Construction;
+import org.graphysica.espace2d.Espace;
 import org.graphysica.vue.AffichageConstruction;
 
-public class Application extends javafx.application.Application {
+/**
+ * Initialise et lance l'application Graphysica.
+ * 
+ * @author Marc-Antoine Ouimet
+ * @author Victor Babin
+ */
+public final class Application extends javafx.application.Application {
 
+    /**
+     * L'ensemble des chemins vers les polices à charger.
+     */
+    private static final String[] POLICES = {
+        "/org/scilab/forge/jlatexmath/fonts/base/jlm_cmmi10.ttf",
+        "/org/scilab/forge/jlatexmath/fonts/maths/jlm_cmsy10.ttf",
+        "/org/scilab/forge/jlatexmath/fonts/latin/jlm_cmr10.ttf"
+    };
+
+    /**
+     * La construction de l'application.
+     */
     private final Construction construction = new Construction();
-
-    @Override
-    public void start(@NotNull final Stage stage) throws Exception {
-        construction.getGestionnaireEspaces().dupliquerEspace();
-        final Scene scene = new Scene(new AffichageConstruction(construction));
-        scene.getStylesheets().add("/styles/styles.css");
-        stage.setTitle("Graphysica");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     /**
      * Lance l'application.
@@ -29,5 +39,38 @@ public class Application extends javafx.application.Application {
     public static void main(@NotNull final String[] arguments) {
         launch(arguments);
     }
-    
+
+    {
+        construction.getEspaces().add(new Espace());
+    }
+
+    @Override
+    public void init() throws Exception {
+        chargerPolices(POLICES);
+    }
+
+    @Override
+    public void start(@NotNull final Stage stage) throws Exception {
+        final Scene scene = new Scene(new AffichageConstruction(construction));
+        scene.getStylesheets().add("/styles/styles.css");
+        stage.setTitle("Graphysica");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream(
+                "/images/graphysica-icone.png")));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Charge un ensemble de polices.
+     *
+     * @param polices l'ensemble des chemins vers les polices à charger.
+     */
+    private static void chargerPolices(@NotNull final String... polices) {
+        final Class classe = Application.class;
+        for (final String police : polices) {
+            javafx.scene.text.Font.loadFont(
+                    classe.getResourceAsStream(police), 12);
+        }
+    }
+
 }
